40ac8cb337fd023e8086f0e3d4bfca3478ae96e4
songsujung <sujung033131@gmail.com>
2023-03-27 18:23:49 +0900

    생성자
    생성자

--- javaProject/Firstjava/src/chapter05/phone/SmartPhone.java
+++ javaProject/Firstjava/src/chapter05/phone/SmartPhone.java
@@ -10,8 +10,27 @@ public class SmartPhone {
 	// 생성자 : 초기화 메소드, 인스턴스 변수의 초기화 작업
 	// 초기화 작업이 없는 경우 생략
 	//아래와 같은 생성자 => 기본생성자
-	SmartPhone(){
-		System.out.println("인스턴스 생성!");
+//	SmartPhone(){
+//		System.out.println("인스턴스 생성!");
+//	}
+	
+	// 초기화를 위한 생성자 정의
+	SmartPhone(String color, float size, int volume) {
+		this.color = color;
+		this.size = size;
+		this.volume = volume;
+	}
+	
+	SmartPhone(String color, float size) {
+		
+		this(color, size, 0); // this() => 다른 생성자의 호출	
+//		this.color = color;
+//		this.size = size;
+//		this.volume = 0;
+	}
+	
+	SmartPhone() {
+		this("white", 4.5f, 5);
 	}
 	
 	void call() {
