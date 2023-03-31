fdf9416b4cf94bad7c35848c42bcf5f400d77f3d
songsujung <sujung033131@gmail.com>
2023-03-31 17:46:53 +0900

    챕터7 예제
    챕터7 예제

---
+++ javaProject/Firstjava/src/exam/chapter07/Female.java
@@ -0,0 +1,28 @@
+package exam.chapter07;
+
+public class Female extends Person{
+
+	private String msg;
+	
+	public Female(String name, String personNum) {
+		super(name, personNum);
+		this.msg = "안녕!";
+		
+	}
+
+	public Female(String name, String personNum, String msg) {
+		super(name, personNum);
+		this.msg = msg;
+	}
+
+	@Override
+	public void printGreeting() {
+		super.printGreeting();
+		System.out.println(msg);
+	}
+	
+	
+	
+	
+	
+}
---
+++ javaProject/Firstjava/src/exam/chapter07/Male.java
@@ -0,0 +1,24 @@
+package exam.chapter07;
+
+public class Male extends Person {
+	
+	String msg;
+	
+	public Male(String name, String personNum, String msg) {
+		super(name, personNum);
+		this.msg = msg;
+
+	}
+
+
+	@Override
+	public void printGreeting() {
+		super.printGreeting();
+		System.out.println(msg);
+	}
+	
+	void hello() {
+		System.out.println("안녕하세요!!!!");
+	}
+	
+}
