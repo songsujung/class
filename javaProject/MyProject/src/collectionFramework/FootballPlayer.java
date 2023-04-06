06595d103134ab2e02288a83d07a3d415ed02cd0
songsujung <sujung033131@gmail.com>
2023-04-06 11:11:11 +0900

    코딩해보기11
    코딩해보기11

--- javaProject/MyProject/src/collectionFramework/FootballPlayer.java
+++ javaProject/MyProject/src/collectionFramework/FootballPlayer.java
@@ -3,7 +3,7 @@ package collectionFramework;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FootballPlayer {
+public class FootballPlayer { // 축구선수의 정보를 저장하는 클래스 : 축구선수 데이터저장 -> 인스턴스 생성
 	/* 축구선수 클래스를 만들어 봅시다.
 		class FootballPlayer {
  		String name;
@@ -14,23 +14,66 @@ public class FootballPlayer {
 	 */
 	// 1. 축구선수 인스턴스를 저장할 수 있는 List<E> 컬렉션 인스턴스를 생성해서 인스턴스를 저장하고 출력하는 프로그램을 만들어 봅시다.
 	
-	String name;
-	int number;
-	String team;
-	int age;
+	//캡슐화, 변수선언
+	private String name;
+	private int number;
+	private String team;
+	private int age;
 
-	public static void main(String[] args) {
-		
-		ArrayList<String> player = new ArrayList<String>();
-		
-		
-		player.add("더브라위너");
-		player.add(new String("필포든"));
-		player.add(new String("홀란드"));
-		player.add(new String("칸셀루"));
-		player.add(new String("에데르송"));
-		
-		
-		
+	public FootballPlayer(String name, int number, String team, int age) { // 생성자
+		super();
+		this.name = name;
+		this.number = number;
+		this.team = team;
+		this.age = age;
 	}
+	
+	public FootballPlayer() {} // 기본생성자
+	
+	
+	// getter,setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "FootballPlayer [name=" + name + ", number=" + number + ", team=" + team + ", age=" + age + "]";
+	}
+
+	
+	public void showInfo() {
+		System.out.printf("[%s] %s(%d, %d)\n", this.team, this.name, this.number, this.age);
+	}
+	
+		
 }
