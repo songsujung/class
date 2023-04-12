3320febb544970ca990081263da6b7533029b258
songsujung <sujung033131@gmail.com>
2023-04-12 20:56:23 +0900

    SQL 3일차. SQL 주요 함수, 그룹함수
    SQL 3일차. SQL 주요 함수, 그룹함수

---
+++ sql/function_exam2.sql
@@ -0,0 +1,21 @@
+--function_exam2
+
+
+--1. 마당서점의고객이요구하는다음질문에대해SQL 문을작성하시오.
+--(1) 박지성의총구매액(박지성의고객번호는1번으로놓고작성)
+
+
+--(2) 박지성이구매한도서의수(박지성의고객번호는1번으로놓고작성)
+
+--2. 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
+--(1) 마당서점도서의총개수
+select count(bookid)
+from book;
+
+--(2) 마당서점에도서를출고하는출판사의총개수
+select publisher,
+        count(*) as "출판사의총개수"
+from book
+group by publisher
+order by publisher
+;
