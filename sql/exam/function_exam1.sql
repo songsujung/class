9636e4e8dcae61138c3353f6f42aa8b34fb10d53
songsujung <sujung033131@gmail.com>
2023-04-13 12:04:54 +0900

    SQL 3일차. SQL 주요 함수, 그룹함수
    SQL 3일차. SQL 주요 함수, 그룹함수

--- sql/exam/function_exam1.sql
+++ sql/exam/function_exam1.sql
@@ -1,44 +1,56 @@
-
+--function_exam1
 
 -- 16. SUBSTR 함수를 사용하여 사원들의 입사한 년도와 입사한 달만 출력하시오.
 select hiredate, substr(hiredate, 1, 5)
-from emp;
+from emp
+;
 
 
 --17. SUBSTR 함수를 사용하여 4월에 입사한 사원을 출력하시오.
-select ename, hiredate 
+select *
 from emp 
-where substr(hiredate, 4, 2) = 04;
+where substr(hiredate, 4, 2) = 04
+;
 
 
 --18. MOD 함수를 사용하여 사원번호가 짝수인 사람만 출력하시오.
-select empno, ename 
+select *
 from emp 
-where mod(empno,2) = 0;
+where mod(empno,2) = 0
+;
 
 
 --19. 입사일을 년도는 2자리(YY), 월은 숫자(MM)로 표시하고 요일은 약어 (DY)로 지정하여 출력하시오.
-select to_char(hiredate, 'YY-MM DY')
-from emp;
+select ename,
+        to_char(hiredate, 'YY.MM. DY') as "입사 년/월/요일"
+--      to_char(hiredate, 'YY') as "입사 년도",
+--      to_char(hiredate, 'MM') as "입사 월",
+--      to_char(hiredate, 'DY') as "입사 요일"
+from emp
+;
 
 
 --20. 올해 며칠이 지났는지 출력하시오. 현재날짜에서 올해 1월 1일을 뺀 결과를 출력하고 TO_DATE 함수를 사용하여 데이터 형을 일치 시키시오.
-select round(sysdate - to_date(20230101, 'yymmdd')) as diffDate
+select sysdate,
+        to_date('2023-01-01'),
+        trunc(sysdate - to_date('2023-01-01', 'YYYY-MM-DD')) as diffDate
 from dual;
 
 --21. 사원들의 상관 사번을 출력하되 상관이 없는 사원에 대해서는 NULL 값 대신 0으로 출력하시오.
-select nvl(mgr, 0) 
-from emp;
+select mgr, nvl(mgr, 0) 
+from emp
+;
 
 --22. DECODE 함수로 직급에 따라 급여를 인상하도록 하시오. 직급이 ‘ANALYST'인 사원은 200, ‘SALESMAN’인 사원은 180, ‘MANAGER’인 사원은 150, ‘CLERK”인 사원은 100을 인상하시오.
-select empno, ename, job, sal, 
+select ename, job, sal, 
         nvl(decode(job,
         'ANALYST', sal+200,
         'SALESMAN', sal+180,
         'MANAGER', sal+150,
         'CLERK', sal+100
         ), sal) salup
-from emp;
+from emp
+;
 
 --23. 모든 사원의 급여 최고액, 최저액, 총액 및 평균 급여를 출력하시오. 평균에 대해서는 정수로 반올림하시오.
 select max(sal),min(sal),sum(sal),round(avg(sal))
@@ -52,7 +64,6 @@ select job,
         round(avg(sal)) as "평균급여액"
 from emp
 group by job
-order by job
 ;
 
 --25. COUNT(*) 함수를 이용하여 담당업무가 동일한 사원 수를 출력하시오.
@@ -60,17 +71,18 @@ select job,
         count(*) as "사원의수"
 from emp
 group by job
-order by job
 ;
         
 
 --26. 관리자 수를 출력하시오.
-select count(mgr)
-from emp;
+select count(distinct mgr)
+from emp
+;
 
 --27. 급여 최고액, 급여 최저액의 차액을 출력하시오.
-select max(sal)- min(sal)
-from emp;
+select max(sal), min(sal), max(sal)- min(sal)
+from emp
+;
 
 
 --28. 직급별 사원의 최저 급여를 출력하시오. 관리자를 알 수 없는 사원과 최저 급여가 2000 미만인 그룹은 제외시키고 결과를 급여에 대한 내림차순으로 정렬하여 출력하시오.
@@ -78,33 +90,37 @@ select job,
         min(sal) as "최저 급여"
 from emp
 where mgr is not null
-
 group by job
 having min(sal) >= 2000
-order by min(sal) desc;
+order by min(sal) desc
+;
 
 
 --29. 각 부서에 대해 부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 소수점 둘째 자리로 반올림 하시오.
 select deptno,
         count(*) as "사원 수",
-        round(avg(sal)) as "사원평균급여"
+        round(avg(sal),2) as "사원평균급여"
 from emp
-group by deptno;
+group by deptno
+order by deptno
+;
 
         
 --30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 정수로 반올림 하시오. DECODE 사용.
-select decode(deptno,
-        10, '부서1',
-        20, '부서2',
-        30, '부서3',
-        40, '부서4'
-        ),
+select deptno,
         decode(deptno,
-        10, 'NEW YORK',
-        20, 'DALLAS',
-        30, 'CHICAGO',
-        40, 'BOSTON'
-        ), count(*), trunc(avg(sal))
+             10, 'ACCOUNTING',
+             20, 'RESEARCH',
+             30, 'SALES',
+             40, 'OPERATIONS'
+         ) as dname,
+        decode(deptno,
+             10, 'NEW YORK',
+             20, 'DALLAS',
+             30, 'CHICAGO',
+             40, 'BOSTON'
+         )as loc,
+         count(*), round(avg(sal))
 from emp
 group by deptno
 order by deptno;
