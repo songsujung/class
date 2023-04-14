4c2e4a1e202de180ea5accf8c8740c006b54df51
songsujung <sujung033131@gmail.com>
2023-04-14 17:56:02 +0900

    SQL 4일차. JOIN
    SQL 4일차. JOIN

--- sql/exam/join_exam1.sql
+++ sql/exam/join_exam1.sql
@@ -2,32 +2,33 @@
 
 
 --32. EQUI 조인을 사용하여 SCOTT 사원의 부서번호와 부서 이름을 출력하시오.
-select emp.ename, emp.deptno, dept.dname
-from emp, dept
-where emp.deptno=dept.deptno
+select e.ename, e.deptno, d.dname
+from emp e, dept d
+where e.deptno=d.deptno
 and ename='SCOTT'
 ;
 
 
 --33. INNER JOIN과 ON 연산자를 사용하여 사원 이름과 함께 그 사원이 소속된 부서이름과 지역 명을 출력하시오.
-select emp.ename, dept.deptno, dept.loc
-from emp inner join dept
-on emp.deptno=dept.deptno
+select e.ename, d.deptno, d.loc
+from emp e inner join dept d
+on e.deptno=d.deptno
 ;
 
 
 --36. 조인과 WildCARD를 사용하여 이름에 ‘A’가 포함된 모든 사원의 이름과 부서명을 출력하시오.
-select emp.ename, dept.dname
-from emp, dept
-where emp.deptno=dept.deptno
-and ename like '%A%'
+select e.ename, d.dname
+from emp e, dept d
+where e.deptno=d.deptno
+and e.ename like '%A%'
 ;
 
 
 --37. JOIN을 이용하여 NEW YORK에 근무하는 모든 사원의 이름, 업무, 부서번호 및 부서명을 출력하시오.
 select e.ename, e.job, e.deptno, d.dname, d.loc
-from emp e join dept d
-on d.loc = 'NEW YORK'
+from emp e, dept d
+where e.deptno=d.deptno
+and d.loc = 'NEW YORK'
 ;
 
 
@@ -47,18 +48,20 @@ order by m.empno desc
 
 
 --40. SELF JOIN을 사용하여 지정한 사원의 이름, 부서번호, 지정한 사원과 동일한 부서에서 근무하는 사원을 출력하시오. ( SCOTT )
-select e.ename, e.deptno, s.deptno, s.ename
+select s.ename, s.deptno
 from emp e, emp s
-where e.deptno = s.deptno
-and e.ename = 'SCOTT'
+where e.deptno = s.deptno 
+and e.ename = 'SCOTT' and s.ename!='SCOTT'
 ;
 
 
 --41. SELF JOIN을 사용하여 WARD 사원보다 늦게 입사한 사원의 이름과 입사일을 출력하시오.
 select e.ename, e.hiredate, w.hiredate
 from emp e, emp w
-where e.hiredate > w.hiredate and w.ename = 'WARD'
+where w.ename = 'WARD'
+and e.hiredate > w.hiredate 
 ;
+
 
 --42. SELF JOIN 을 사용하여 관리자보다 먼저 입사한 모든 사원의 이름 및 입사일을 관리자의 이름 및 입사일과 함께 출력하시오.
 select e.ename, e.hiredate, m.hiredate, m.ename
