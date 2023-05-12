cddbd112f5d5cd9247aaa43dcb91e2d6541664bc
songsujung <sujung033131@gmail.com>
2023-05-12 18:42:05 +0900

    SpringMVC
    SpringMVC

--- springProject/intellij/SpringMVC/src/main/java/com/hi/app/controller/SearchController.java
+++ springProject/intellij/SpringMVC/src/main/java/com/hi/app/controller/SearchController.java
@@ -5,12 +5,15 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Log4j2
@@ -50,5 +53,26 @@ public class SearchController {
 
         return "search/search1";
     }
+
+    @RequestMapping("search/main")
+    public String main() {
+        return "search/main";
+    }
+
+    @ModelAttribute("keywords")
+    public String[] getQueryList() {
+        return new String[] {"손흥민", "이강인", "김민재"};
+    }
+
+    @ModelAttribute("searchType")
+    public List<String> getSearchType() {
+        List<String> list = new ArrayList<>();
+        list.add("id");
+        list.add("title");
+        list.add("keyword");
+
+        return list;
+    }
+
 
 }
