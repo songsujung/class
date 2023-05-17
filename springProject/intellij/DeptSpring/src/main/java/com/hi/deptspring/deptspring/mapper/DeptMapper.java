b70330fff1c67fa83b9dab52df5610366f34ed24
songsujung <sujung033131@gmail.com>
2023-05-17 17:53:01 +0900

    DeptSpring
    DeptSpring

--- springProject/intellij/DeptSpring/src/main/java/com/hi/deptspring/deptspring/mapper/DeptMapper.java
+++ springProject/intellij/DeptSpring/src/main/java/com/hi/deptspring/deptspring/mapper/DeptMapper.java
@@ -2,6 +2,7 @@ package com.hi.deptspring.deptspring.mapper;
 
 import com.hi.deptspring.deptspring.domain.DeptDTO;
 import com.hi.deptspring.deptspring.domain.DeptRegistRequest;
+import com.hi.deptspring.deptspring.domain.DeptSearchOption;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,11 +13,20 @@ public interface DeptMapper {
     // 전체 부서 리스트.
     List<DeptDTO> selectAll();
 
+    List<DeptDTO> selectAll2();
+
+    // 검색어로 검색 결과를 반환
+    List<DeptDTO> selectByOption(DeptSearchOption option);
+
     // deptno 컬럼으로 검색 => DeptDTO
     DeptDTO selectByDeptno(int deptno);
 
+    List<DeptDTO> selectByDeptnos(List<Integer> deptnos);
+
     // DeptRegistRequest를 받아서 insert 해주는 메소드
     int insertDept(DeptRegistRequest request);
+
+    int insertDept2(DeptDTO deptDTO);
 
     // DeptDTO를 전달 받고 utdate하는 메소드
     int updateDept(DeptDTO deptDTO);
